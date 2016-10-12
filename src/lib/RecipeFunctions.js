4a7ace71cc41e49702eae1b6f314eab700142159
Elvey <Matthew.Elvey@cdu.edu.au>
2016-10-12 14:50:35 +0930

    Add get ppg and ppKgPl with tests

--- src/lib/RecipeFunctions.js
+++ src/lib/RecipeFunctions.js
@@ -17,6 +17,46 @@ function calculateGravity(recipe) {
 	return gravity;
 }
 
+/** 
+ * get points per kilogram per litre
+ * @param {Object} recipe The recipe to calculate ppKgPl for
+ * @param {Object} options Configuration/environmental options
+ * @return {number} points per kilogram per litre for this recipe
+ **/
+function getPpKgPl(recipe, options = {}) {
+	const defaultEfficency = 0.85;
+	const ppgToMetricConversion = 8.316;
+	const potentialToPoints = 1000;
+
+	const efficiency = options.efficiency || defaultEfficency;
+
+	const points = recipe.grains.reduce((last, elem) => {
+		return last + ((elem.potential - 1) * potentialToPoints * ppgToMetricConversion * elem.weight);
+	}, 0);
+
+	const availablePoints = points * efficiency;
+	return (availablePoints / recipe.volume) * ppgToMetricConversion; // Can't work out for the life of my why this conversion has to be done twice
+}
+function getPpg(recipe, options = {}) {
+	const defaultEfficency = 0.85;
+	const kgToLb = 2.20462;
+	const litreToGallon = 0.264172;
+	const potentialToPoints = 1000;
+
+	const efficiency = options.efficiency || defaultEfficency;
+
+	const points = recipe.grains.reduce((last, elem) => {
+		const lbs = elem.weight * kgToLb;
+		return last + ((elem.potential - 1) * potentialToPoints * lbs);
+	}, 0);
+	console.log(points);
+
+	const availablePoints = points * efficiency;
+	return availablePoints / (recipe.volume * litreToGallon);
+}
+
 export {calculateGravity};
+export {getPpKgPl};
+export {getPpg};
 
 export default {calculateGravity};
--- test/TestRecipeFunctions.js
+++ test/TestRecipeFunctions.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {calculateGravity} from '../src/lib/RecipeFunctions';
+import {calculateGravity, getPpKgPl, getPpg} from '../src/lib/RecipeFunctions';
 
 describe('Calculate gravity', () => {
 	it('Calculate gravity for single malt', () => {
@@ -19,5 +19,37 @@ describe('Calculate gravity', () => {
 
 		const gravity = calculateGravity(recipe);
 		expect(gravity).to.be.equal(1.055);
+	});
+
+	it('Calculate gravity points', () => {
+		// Using recipe from http://brewgr.com/calculations/original-gravity
+		const recipe = {
+			volume: 18.9271,
+			grains: [
+				{
+					name: 'Pale Malt',
+					ebc: 4,
+					potential: 1.036,
+					weight: 3.62874 // 8lbs
+				},
+				{
+					name: 'Roast Barley',
+					potential: 1.025,
+					weight: 0.453592 // 1lb
+				}
+			],
+			adjuncts: [],
+			hops: []
+		};
+		const options = {
+			efficiency: 0.72
+		};
+
+		const gravityPoints = getPpKgPl(recipe, options);
+		// 44.8 ppg === 372.5568 ppKgPl
+		const ppg = 44.8;
+		const ppKgPl = 372.5568;
+		expect(getPpKgPl(recipe, options)).to.be.within(ppKgPl * 0.99, ppKgPl * 1.01);
+		expect(getPpg(recipe, options)).to.be.within(ppg * 0.99, ppg * 1.01);
 	});
 });
