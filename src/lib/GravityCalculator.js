d48c23cda38437563671076912f7de4b7122d6b0
Elvey <matt.elvey@gmail.com>
2016-10-27 08:21:53 +0930

    Add plato gravity calculation

--- src/lib/GravityCalculator.js
+++ src/lib/GravityCalculator.js
@@ -1,11 +1,60 @@
+const defaultEfficiency = 0.85;
+const defaultDryWeight = 0.97; // Apparently most grains are 3% water which is not factored into their potantial
 
-function getOriginalGravity(recipe, options = {}) {
-	const defaultEfficency = 0.85;
-	const defaultDryWeight = 0.97; // Apparently most grains are 3% water which is not factored into their potantial
+/**
+ * Get plato for a given grain profile
+ * @param {Object} recipe The recipe to calculate plato for
+ * @param {array} recipe.grains Array of grain data to calculate plato from
+ * @param {number} recipe.volume The total volume of the wort (or the amount of water added if options.addGrainVolume is true)
+ * @param {Object} options Configuration options for this calculation
+ * @param {boolean} options.addGrainVolume Should the volume of grain be added to the wort volume (set to true if this isn't already factored in)
+ * @param {number} options.dryWeight The dry weight percent of the grain
+ * @param {number} options.efficiency The extraction efficiency of the boil
+ * @return {number} plato value of the recipe
+ **/
+function getPlato(recipe, options = {}) {
+	const platoFactor = 100;
 
+	const dryWeight = options.dryWeight || defaultDryWeight;
+	const efficiency = options.efficiency || defaultEfficiency;
+
+	const extractMass = recipe.grains.reduce((last, grain) => {
+		const grainMass = grain.weight * dryWeight * efficiency;
+		return last + grainMass;
+	}, 0);
+
+	const finalWeight = options.addGrainVolume ? recipe.volume + extractMass : recipe.volume;
+
+	return platoFactor * (extractMass / finalWeight);
+}
+
+/**
+* Convert plato values to specific gravity
+* Formula from http://www.brewersfriend.com/plato-to-sg-conversion-chart/
+* It would be nice to identify and label what these magic numbers are
+* @param {number} plato The plato value for the wort
+* @return {number} The specific gravity value equilivent to the provided plato value
+**/
+function platoToSG(plato) {
+	return 1 + (plato / (258.6 - ((plato / 258.2) * 227.1))); // eslint-disable-line no-magic-numbers
+}
+
+/**
+ * Gravity calculation from ianh's spreadsheet at http://aussiehomebrewer.com/topic/55955-biab-beer-designer-spreadsheet/
+ * Looks to be a plato calculation although there is a division at the end I'm not sure about.
+ * @param {Object} recipe The recipe to calculate plato for
+ * @param {array} recipe.grains Array of grain data to calculate plato from
+ * @param {number} recipe.volume The total volume of the wort (or the amount of water added if options.addGrainVolume is true)
+ * @param {Object} options Configuration options for this calculation
+ * @param {boolean} options.addGrainVolume Should the volume of grain be added to the wort volume (set to true if this isn't already factored in)
+ * @param {number} options.dryWeight The dry weight percent of the grain
+ * @param {number} options.efficiency The extraction efficiency of the boil
+ * @return {number} original gravity of the recipe
+ **/
+function ianhGravity(recipe, options) {
 	let gravity = 1;
 	if(recipe.volume > 0) {
-		const efficency = options.efficency || defaultEfficency;
+		const efficiency = options.efficiency || defaultEfficiency;
 		const dryWeight = options.dryWeight || defaultDryWeight;
 		const platoFactor = 100;	// Platos are the grain potential in weight * 100
 		const gravityFactor = 10; // No idea where this comes from.
@@ -17,7 +66,7 @@ function getOriginalGravity(recipe, options = {}) {
 			const grainPlato = (potential - 1) * elem.weight * platoFactor;
 			plato += grainPlato;
 		});
-		gravity = 1 + ((efficency * (plato / recipe.volume)) / gravityFactor);
+		gravity = 1 + ((efficiency * (plato / recipe.volume)) / gravityFactor);
 	}
 	return gravity;
 }
@@ -62,11 +111,11 @@ function getFinalGravity(og, yeast) {
  * @return {number} points per kilogram per litre for this recipe
  **/
 function getPpKgPl(recipe, options = {}) {
-	const defaultEfficency = 0.85;
+	const defaultEfficiency = 0.85;
 	const ppgToMetricConversion = 8.316;
 	const potentialToPoints = 1000;
 
-	const efficiency = options.efficiency || defaultEfficency;
+	const efficiency = options.efficiency || defaultEfficiency;
 
 	const points = recipe.grains.reduce((last, elem) =>
 		last + ((elem.potential - 1) * potentialToPoints * ppgToMetricConversion * elem.weight)
@@ -77,12 +126,12 @@ function getPpKgPl(recipe, options = {}) {
 }
 
 function getPpg(recipe, options = {}) {
-	const defaultEfficency = 0.85;
+	const defaultEfficiency = 0.85;
 	const kgToLb = 2.20462;
 	const litreToGallon = 0.264172;
 	const potentialToPoints = 1000;
 
-	const efficiency = options.efficiency || defaultEfficency;
+	const efficiency = options.efficiency || defaultEfficiency;
 
 	const points = recipe.grains.reduce((last, elem) => {
 		const lbs = elem.weight * kgToLb;
@@ -93,9 +142,17 @@ function getPpg(recipe, options = {}) {
 	return availablePoints / (recipe.volume * litreToGallon);
 }
 
+function getOriginalGravity(recipe, options = {}) {
+	//return platoToSG(getPlato(recipe, options));
+	return ianhGravity(recipe, options);
+}
+
 
 export {getOriginalGravity};
 export {getFinalGravity};
+export {getAlcohol}
+export {getPlato};
+export {platoToSG};
 export {getPpKgPl};
 export {getPpg};
 
--- test/TestGravity.js
+++ test/TestGravity.js
@@ -1,6 +1,6 @@
 /* eslint no-magic-numbers: 0 */
 import {expect} from 'chai';
-import {getOriginalGravity, getFinalGravity} from '../src/lib/GravityCalculator';
+import {getOriginalGravity, getFinalGravity, getPlato, platoToSG} from '../src/lib/GravityCalculator';
 
 /**
 * Rount a float to the given number of decimal places
@@ -13,7 +13,119 @@ function roundToDecimal(number, places = 3) {
 	return Math.round(number * multiplier) / multiplier;
 }
 
+function expectWithinMargin(value, target, margin) {
+	expect(value).to.be.within(value - margin, value + margin);
+}
+
 describe('Calculate gravity', () => {
+	it('Calculate degrees plato', () => {
+		// Calculation from http://www.homebrewtalk.com/showthread.php?t=540961
+		const recipe = {
+			volume: 3.785,
+			grains: [
+				{
+					name: 'Pale Malt',
+					ebc: 4,
+					potential: 1.36288,
+					weight: 0.4536
+				}
+			],
+			adjuncts: [],
+			hops: []
+		};
+		const settings = {
+			efficiency: 0.8,
+			dryWeight: 1,
+			addGrainVolume: true
+		};
+
+		const plato = roundToDecimal(getPlato(recipe, settings), 2);
+		expect(plato).to.be.equal(8.75);
+	});
+
+	it('Convert plato to SG', () => {
+		expect(roundToDecimal(platoToSG(0.5))).to.be.equal(1.002);
+			expect(roundToDecimal(platoToSG(1))).to.be.equal(1.004);
+			expect(roundToDecimal(platoToSG(1.5))).to.be.equal(1.006);
+			expect(roundToDecimal(platoToSG(2))).to.be.equal(1.008);
+			expect(roundToDecimal(platoToSG(2.5))).to.be.equal(1.010);
+			expect(roundToDecimal(platoToSG(3))).to.be.equal(1.012);
+			expect(roundToDecimal(platoToSG(3.5))).to.be.equal(1.014);
+			expect(roundToDecimal(platoToSG(4))).to.be.equal(1.016);
+			expect(roundToDecimal(platoToSG(4.5))).to.be.equal(1.018);
+			expect(roundToDecimal(platoToSG(5))).to.be.equal(1.020);
+			expect(roundToDecimal(platoToSG(5.5))).to.be.equal(1.022);
+			expect(roundToDecimal(platoToSG(6))).to.be.equal(1.024);
+			expect(roundToDecimal(platoToSG(6.5))).to.be.equal(1.026);
+			expect(roundToDecimal(platoToSG(7))).to.be.equal(1.028);
+			expect(roundToDecimal(platoToSG(7.5))).to.be.equal(1.030);
+			expect(roundToDecimal(platoToSG(8))).to.be.equal(1.032);
+			expect(roundToDecimal(platoToSG(8.5))).to.be.equal(1.034);
+			expect(roundToDecimal(platoToSG(9))).to.be.equal(1.036);
+			expect(roundToDecimal(platoToSG(9.5))).to.be.equal(1.038);
+			expect(roundToDecimal(platoToSG(10))).to.be.equal(1.040);
+			expect(roundToDecimal(platoToSG(10.5))).to.be.equal(1.042);
+			expect(roundToDecimal(platoToSG(11))).to.be.equal(1.044);
+			expect(roundToDecimal(platoToSG(11.5))).to.be.equal(1.046);
+			expect(roundToDecimal(platoToSG(12))).to.be.equal(1.048);
+			expect(roundToDecimal(platoToSG(12.5))).to.be.equal(1.050);
+			expect(roundToDecimal(platoToSG(13))).to.be.equal(1.053);
+			expect(roundToDecimal(platoToSG(13.5))).to.be.equal(1.055);
+			expect(roundToDecimal(platoToSG(14))).to.be.equal(1.057);
+			expect(roundToDecimal(platoToSG(14.5))).to.be.equal(1.059);
+			expect(roundToDecimal(platoToSG(15))).to.be.equal(1.061);
+			expect(roundToDecimal(platoToSG(15.5))).to.be.equal(1.063);
+			expect(roundToDecimal(platoToSG(16))).to.be.equal(1.065);
+			expect(roundToDecimal(platoToSG(16.5))).to.be.equal(1.068);
+			expect(roundToDecimal(platoToSG(17))).to.be.equal(1.070);
+			expect(roundToDecimal(platoToSG(17.5))).to.be.equal(1.072);
+			expect(roundToDecimal(platoToSG(18))).to.be.equal(1.074);
+			expect(roundToDecimal(platoToSG(18.5))).to.be.equal(1.076);
+			expect(roundToDecimal(platoToSG(19))).to.be.equal(1.079);
+			expect(roundToDecimal(platoToSG(19.5))).to.be.equal(1.081);
+			expect(roundToDecimal(platoToSG(20))).to.be.equal(1.083);
+			expect(roundToDecimal(platoToSG(20.5))).to.be.equal(1.085);
+			expect(roundToDecimal(platoToSG(21))).to.be.equal(1.087);
+			expect(roundToDecimal(platoToSG(21.5))).to.be.equal(1.090);
+			expect(roundToDecimal(platoToSG(22))).to.be.equal(1.092);
+			expect(roundToDecimal(platoToSG(22.5))).to.be.equal(1.094);
+			expect(roundToDecimal(platoToSG(23))).to.be.equal(1.096);
+			expect(roundToDecimal(platoToSG(23.5))).to.be.equal(1.099);
+			expect(roundToDecimal(platoToSG(24))).to.be.equal(1.101);
+			expect(roundToDecimal(platoToSG(24.5))).to.be.equal(1.103);
+			expect(roundToDecimal(platoToSG(25))).to.be.equal(1.106);
+			expect(roundToDecimal(platoToSG(25.5))).to.be.equal(1.108);
+			expect(roundToDecimal(platoToSG(26))).to.be.equal(1.110);
+			expect(roundToDecimal(platoToSG(26.5))).to.be.equal(1.113);
+			expect(roundToDecimal(platoToSG(27))).to.be.equal(1.115);
+			expect(roundToDecimal(platoToSG(27.5))).to.be.equal(1.117);
+			expect(roundToDecimal(platoToSG(28))).to.be.equal(1.120);
+			expect(roundToDecimal(platoToSG(28.5))).to.be.equal(1.122);
+			expect(roundToDecimal(platoToSG(29))).to.be.equal(1.124);
+			expect(roundToDecimal(platoToSG(29.5))).to.be.equal(1.127);
+			expect(roundToDecimal(platoToSG(30))).to.be.equal(1.129);
+			expect(roundToDecimal(platoToSG(30.5))).to.be.equal(1.132);
+			expect(roundToDecimal(platoToSG(31))).to.be.equal(1.134);
+			expect(roundToDecimal(platoToSG(31.5))).to.be.equal(1.136);
+			expect(roundToDecimal(platoToSG(32))).to.be.equal(1.139);
+			expect(roundToDecimal(platoToSG(32.5))).to.be.equal(1.141);
+			expect(roundToDecimal(platoToSG(33))).to.be.equal(1.144);
+			expect(roundToDecimal(platoToSG(33.5))).to.be.equal(1.146);
+			expect(roundToDecimal(platoToSG(34))).to.be.equal(1.149);
+			expect(roundToDecimal(platoToSG(34.5))).to.be.equal(1.151);
+			expect(roundToDecimal(platoToSG(35))).to.be.equal(1.154);
+			expect(roundToDecimal(platoToSG(35.5))).to.be.equal(1.156);
+			expect(roundToDecimal(platoToSG(36))).to.be.equal(1.159);
+			expect(roundToDecimal(platoToSG(36.5))).to.be.equal(1.161);
+			expect(roundToDecimal(platoToSG(37))).to.be.equal(1.164);
+			expect(roundToDecimal(platoToSG(37.5))).to.be.equal(1.166);
+			expect(roundToDecimal(platoToSG(38))).to.be.equal(1.169);
+			expect(roundToDecimal(platoToSG(38.5))).to.be.equal(1.171);
+			expect(roundToDecimal(platoToSG(39))).to.be.equal(1.174);
+			expect(roundToDecimal(platoToSG(39.5))).to.be.equal(1.176);
+			expect(roundToDecimal(platoToSG(40))).to.be.equal(1.179);
+	});
+
 	it('Calculate gravity for single malt', () => {
 		let recipe = {
 			volume: 5,
@@ -37,7 +149,6 @@ describe('Calculate gravity', () => {
 
 		expect(gravity).to.be.equal(1.055);
 
-		/*
 		// Calculation from http://www.homebrewtalk.com/showthread.php?t=540961
 		recipe = {
 			volume: 3.785,
@@ -53,14 +164,14 @@ describe('Calculate gravity', () => {
 			hops: []
 		};
 		settings = {
-			efficiency: 0.85,
-			dryWeight: 0.994837
+			efficiency: 0.80,
+			dryWeight: 1
 		};
 
+		console.log(getOriginalGravity(recipe, settings));
 		gravity = Math.round(getOriginalGravity(recipe, settings) * 1000) / 1000;
-
-		expect(gravity).to.be.equal(1.037);
-		*/
+		console.log(gravity);
+		expectWithinMargin(gravity, 1.037, 0.001);
 	});
 
 });
