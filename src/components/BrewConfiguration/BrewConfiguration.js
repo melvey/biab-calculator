736b15c1f1d47e6431d949b5ef19536c21bd482b
Elvey <matt.elvey@gmail.com>
2016-10-27 08:54:03 +0930

    Add configuration component

---
+++ src/components/BrewConfiguration/BrewConfiguration.js
@@ -0,0 +1,59 @@
+import React, {Component, PropTypes} from 'react';
+import styles from './BrewConfiguration.scss';
+
+class BrewConfiguration extends Component {
+
+	static propTypes = {
+		className: PropTypes.string,
+		settings: PropTypes.shape({
+			efficiency: PropTypes.number,
+			grainDryWeight: PropTypes.number,
+			noChillTime: PropTypes.number
+		}),
+		updateFunc: PropTypes.func.isRequired
+	};
+
+	constructor(props) {
+		super();
+
+		this.props = props;
+		this.state = {
+			efficiencyId: 'brew_configuration-efficiency-input',
+			dryWeightId: 'brew_configuration-dryweight-input',
+			nochillId: 'brew_configuration-nochill-input'
+		};
+	}
+
+	updateEfficiency(evt) {
+		this.props.updateFunc(Object.assign({}, this.props.settings, {efficiency: evt.target.value}));
+	}
+	updateDryWeight(evt) {
+		this.props.updateFunc(Object.assign({}, this.props.settings, {grainDryWeight: evt.target.value}));
+	}
+	updateNoChill(evt) {
+		this.props.updateFunc(Object.assign({}, this.props.settings, {noChillTime: evt.target.value}));
+	}
+
+	render() {
+		const className = this.props.className ? `${styles.content} ${this.props.className}` : styles.content;
+		return (
+			<div className={className}>
+				<div className={styles.row}>
+					<label htmlFor={this.state.efficiencyId}>Efficiency %</label>
+					<input type="number" id={this.state.efficiencyID} value={this.props.settings.efficiency} onChange={this.updateEfficiency} />
+				</div>
+				<div className={styles.row}>
+					<label htmlFor={this.state.dryWeightId}>Grain Dry Weight %</label>
+					<input type="number" id={this.state.dryWeightId} value={this.props.settings.grainDryWeight} onChange={this.updateDryWeight} />
+				</div>
+				<div className={styles.row}>
+					<label htmlFor={this.state.nochillId}>No chill time factor</label>
+					<input type="number" id={this.state.nochillId} value={this.props.settings.noChillTime} onChange={this.updateNoChill} />
+				</div>
+			</div>
+		);
+	}
+
+}
+
+export default BrewConfiguration;
