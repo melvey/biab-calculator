fe95345192dc03e62c0ed9b4974616198c673ad2
Elvey <matt.elvey@gmail.com>
2016-11-10 09:07:20 +0930

    Fix import script

--- scripts/setupdb.js
+++ scripts/setupdb.js
@@ -1,14 +1,24 @@
 import mongoose from 'mongoose';
+import config from '../src/config/mongo';
 import grains from '../src/data/grains.json';
 import Grain from '../src/models/Grain';
 
 mongoose.Promise = global.Promise;
+mongoose.connection.on('error', console.error);
 
-grains.forEach((grainData) => {
-	var grainModel = new Grain(grainData);
-	grainModel.save().then((result) => {
-		console.log(result);
-	}).catch((err) => {
-		console.log(err);
+
+mongoose.connection.once('open', () => {
+	grains.forEach((grainData) => {
+		var grainModel = new Grain(grainData);
+		grainModel.save((err) => {
+			if(!err) {
+			console.log(grainModel);
+		} else { 
+			console.error(err);
+		}
+		});
 	});
 });
+
+const connection = mongoose.connect(config);
+
